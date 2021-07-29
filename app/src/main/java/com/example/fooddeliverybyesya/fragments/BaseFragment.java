ee1b6d65d95cb791307d173ae6f32094c5b9f6de
Esya37 <User@DESKTOP-KQ4T0UJ>
2021-07-29 16:56:27 +0300

    Класс BaseFragment добавлен в гит

---
+++ app/src/main/java/com/example/fooddeliverybyesya/fragments/BaseFragment.java
@@ -0,0 +1,25 @@
+package com.example.fooddeliverybyesya.fragments;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import androidx.fragment.app.Fragment;
+
+public class BaseFragment extends Fragment {
+
+    public static boolean hasConnection(final Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        return wifiInfo != null && wifiInfo.isConnected();
+    }
+
+}
